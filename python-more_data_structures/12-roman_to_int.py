6d6fda0a8c552b5b8954cad3a25f6b4580e38c76
zehraadgozel <zehraadgozelova@example.com>
2025-06-21 18:55:36 +0000

    s

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    i = 0
+    while i < len(roman_string):
+        # Əgər növbəti simvol var və kiçik dəyər əvvəl gəlirsə
+        if (i + 1 < len(roman_string) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            total += roman_dict[roman_string[i + 1]] - roman_dict[roman_string[i]]
+            i += 2
+        else:
+            total += roman_dict[roman_string[i]]
+            i += 1
+
+    return total
