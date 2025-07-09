b70c6d20cfe4966e458102ef4ecd8c38448f1975
zehra <zehraadgozelova.com>
2025-07-09 15:07:37 +0000

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[0:6])
+print(object.__name__ + "-" + type('').__name__ + " programming with Python")
