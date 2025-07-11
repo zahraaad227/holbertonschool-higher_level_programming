5b0ae299229eda18999082529ef6da556fa55287
zehraa <zehraadgozelova.com>
2025-07-11 14:19:39 +0000

    s

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python3
-from task_00_basic_serialization import load_and_deserialize, serialize_and_save_to_file
+import json
 
-data_to_serialize = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York"
-}
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-serialize_and_save_to_file(data_to_serialize, 'data.json')
-print("Data serialized and saved to 'data.json'.")
-
-deserialized_data = load_and_deserialize('data.json')
-print("Deserialized Data:")
-print(deserialized_data)
+def load_and_deserialize(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
