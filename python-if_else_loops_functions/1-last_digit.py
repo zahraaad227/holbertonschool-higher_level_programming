60d1f6a60a4e17da2e5b62104b06ac9ce6851745
zahraadgozal <zehraadgozelova@example.com>
2025-07-22 14:57:58 +0000

    s

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
+n = number
 ld = abs(number) % 10
+
 if ld > 5:
     if number > 0:
-        print(
-                "Last digit of", number, "is", ld, "and "
-                        "is greater than 5"
-                )
+        print(f"Last digit of {n} is {ld} and is greater than 5")
     else:
-        print(
-                "Last digit of", number, "is", (-1)*ld, "and " 
-                        "is less than 6 and not 0"
-                        )
+        # Burada f-string ilə çap etmək lazımdır, indi isə düzəltdim
+        print(f"Last digit of {number} is {-ld} and is less than 6 and not 0")
 elif ld == 0:
-    print(
-            "Last digit " 
-                    "of", number, "is", ld, "and is 0"
-                    )
+    print(f"Last digit of {n} is {ld} and is 0")
 else:
     if number > 0:
-        print(
-        "Last digit " 
-                "of", number, "is", ld, "and is less than 6 and not 0"
-                )
+        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
     else:
-        print(
-                "Last digit " 
-                        "of", number, "is", (-1)*ld, "and is less than 6 and not 0"
-                        )
+        print(f"Last digit of {number} is {-ld} and is less than 6 and not 0")
