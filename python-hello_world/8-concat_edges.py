7607ab72303cdd0e4e5842d0e1e3dd1f71d066fa
zahraadgozal <zehraadgozelova@example.com>
2025-07-22 13:13:03 +0000

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str1[39:67] + str1[107:113] + str1[:6])
+print(str[39:67]+str[107:112]+str[0:7])
