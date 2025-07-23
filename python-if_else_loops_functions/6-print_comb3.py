05fa0aa8b8597f0f262e775044bf2bdf6728ae42
zehraadgozel <zehraadgozelova@example.com>
2025-07-23 22:16:47 +0000

    s

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0,10):
     for j in range(0,10):
         if i < j:
             if i != 8 or j != 9:
-                print("{:01d}{:01d}, ".format(i,j), end"")
+                print("{:01d}{:01d}, ".format(i,j), end="")
             else:
                 print("{:01d}{:01d}".format(i,j))
