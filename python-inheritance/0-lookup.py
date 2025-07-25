51989832dd232ef64a26ebec298f16fd70425ad7
zehra <zehraadgozelova@example.com>
2025-07-25 12:41:43 +0000

    s

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function that returns the list of available attributes and methods of an object"""
-def lookup(obj):
+"""that returns the list of attributes and methods of an object"""
+def  lookup(obj):
     """listing attributes and methods of an object"""
     return dir(obj)
