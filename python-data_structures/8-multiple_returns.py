7590254b3cd49ea39f995d596bcb62124d78b80a
zehra <zehraad@example.com>
2025-07-25 05:20:22 +0000

    s

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0] if length > 0 else  None
-    return(length, first_char)
+    if length > 0:
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (length, first_char)
