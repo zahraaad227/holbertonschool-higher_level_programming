d4b60f9caf381bcae68446350bda46e04bb8dd16
zehraa <zehraadgozel@example.com>
2025-07-27 12:44:42 +0000

    s

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write that returns True if the obj is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Write a function that returns"""
+    return isinstance(obj, a_class)
