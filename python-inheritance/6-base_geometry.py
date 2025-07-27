328a98c7a954d558c2c29154411ce51f627003e1
zehraa <zehraadgozel@example.com>
2025-07-27 13:11:43 +0000

    s

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/ython3
+"""rite a class BaseGeometry (based on 5"""
+class BaseGeometry:
+"""Public instance method: """
+
+
+    def area(self):
+        raise Exception("area() is not implemented")
