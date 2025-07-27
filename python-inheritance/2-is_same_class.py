45cfb68a6f84801a7bacb62ce35f054f3b5981f7
zehraa <zehraadgozel@example.com>
2025-07-27 12:41:48 +0000

    s git push

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-    """Returns True if the object is exactly an instance of the specified class"""
+    """Returns True if the obj is exac an ins of the specified class"""
     return type(obj) is a_class
