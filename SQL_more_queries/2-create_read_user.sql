721738ff786f1793489eee9347945eb532a501b9
zahra <zzzz@example.com>
2025-07-29 20:48:22 +0000

    s

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,8 @@
--- sql
-CREATE DATABASE IF NOT EXISTS 2-create_read_user.sql;
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist, with the given password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on the hbtn_0d_2 database to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
