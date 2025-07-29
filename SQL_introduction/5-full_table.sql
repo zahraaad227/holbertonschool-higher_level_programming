35b5e36d36631f71b1c9d5aafdb265ee5bdbc9eb
zehraadgozel <zehraadgozelova@example.com>
2025-07-29 09:47:52 +0000

    s

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,22 @@
 --sql
+#!/bin/bash
+
+# Verilənlər bazası adı
 DB_NAME="$1"
-cat 5-full_table.sql | mysql -hlocalhost -uroot -p $DB_NAME
+
+# SQL faylını oxu və MySQL serverinə göndər
+cat 5-full_table.sql | mysql -hlocalhost -uroot -proot $DB_NAME | \
+grep -v 'Using a password' | \
+sed 's/\\n/\n/g' | \
+sed 's/ //g' | \
+sed 's/\t//g' | \
+grep 'first_table' | \
+grep 'AUTO_INCREMENT' | \
+grep 'varchar' | \
+grep 'char(1)' | \
+grep 'created_at' | \
+grep 'PRIMARY' | \
+grep 'ENGINE' > table_desc
+
+# Nəticəni yoxlamaq üçün
+cat table_desc
