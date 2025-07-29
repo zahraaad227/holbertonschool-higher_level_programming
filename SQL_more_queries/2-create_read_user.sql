9371c561c898390598195865c21188eca631799e
zahra <zzzz@example.com>
2025-07-29 20:46:08 +0000

    s

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- sql
+CREATE DATABASE IF NOT EXISTS 2-create_read_user.sql;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
