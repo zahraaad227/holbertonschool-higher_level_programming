c2f8317d8a0fcd53d97f72fdbe8d718fa2caeea6
zehraadgozel <zehraadgozelova@example.com>
2025-07-29 09:45:53 +0000

    s

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,3 @@
 --sql
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(128) DEFAULT NULL,
-	c CHAR(1) DEFAULT NULL,
-	created_at DATE DEFAULT NULL,
-	PRIMARY KEY (id))
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 	
+DB_NAME="$1"
+cat 5-full_table.sql | mysql -hlocalhost -uroot -p $DB_NAME
