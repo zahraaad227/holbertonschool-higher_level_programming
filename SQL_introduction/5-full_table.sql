be6069187b25954baadaf8ebf339b0ae90365b29
zehraadgozel <zehraadgozelova@example.com>
2025-07-29 10:24:54 +0000

    s

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,19 +4,29 @@
 # Verilənlər bazası adı
 DB_NAME="$1"
 
-# SQL faylını oxu və MySQL serverinə göndər
+# SQL faylını oxuyub MySQL serverinə göndəririk
 cat 5-full_table.sql | mysql -hlocalhost -uroot -proot $DB_NAME | \
 grep -v 'Using a password' | \
+# Yeni sətirləri əvəz edin
 sed 's/\\n/\n/g' | \
+# Bütün boşluqları çıxarın
 sed 's/ //g' | \
+# Tabulatorları çıxarın
 sed 's/\t//g' | \
+# `first_table` ilə başlayan cədvəli tapın
 grep 'first_table' | \
+# AUTO_INCREMENT olan sətirləri tapın
 grep 'AUTO_INCREMENT' | \
+# VARCHAR tipi olan sətirləri tapın
 grep 'varchar' | \
+# CHAR(1) tipi olan sətirləri tapın
 grep 'char(1)' | \
+# created_at sütununu tapın
 grep 'created_at' | \
+# PRIMARY KEY açarını tapın
 grep 'PRIMARY' | \
+# ENGINE açarını tapın
 grep 'ENGINE' > table_desc
 
-# Nəticəni yoxlamaq üçün
+# Nəticəni ekranda göstərin
 cat table_desc
