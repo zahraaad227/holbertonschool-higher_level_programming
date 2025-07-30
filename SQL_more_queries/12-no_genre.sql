462bda3faf2bc509dbdf379ffc16e2b84ace6437
zehraaa <zehraadgozal@example.com>
2025-07-30 18:03:20 +0000

    s

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- sql
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
